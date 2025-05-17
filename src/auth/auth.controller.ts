cc0bd0a434683873caa9766afe814ea4807db5a8
Edmilzon <edmilzon.luna@gmail.com>
2025-05-17 00:48:54 -0400

    insert service user

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,18 @@
+import { AuthService } from './auth.service';
 import { Controller, Post } from '@nestjs/common';
 
 @Controller('auth')
 export class AuthController {
+
+	constructor( private readonly authService: AuthService){}
+
 	@Post('register')
 	register() {
-		return 'register';
+		return this.authService.register();
 	}
 
 	@Post('login')
   login() {
-		return 'login';
+		return this.authService.login();
   }
 }
